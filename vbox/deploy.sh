b117fa34b694d7439c4d6e383f79533851d0ae55
cappyzawa <m153304w@st.u-gakugei.ac.jp>
2018-04-07 16:20:55 +0900

    change default check_every for resources

--- vbox/deploy.sh
+++ vbox/deploy.sh
@@ -6,6 +6,7 @@ bosh -e vbox deploy -d concourse ../concourse-bosh-deployment/cluster/concourse.
 --vars-file ./passwords.yml \
 -o ../concourse-bosh-deployment/cluster/operations/basic-auth.yml \
 -o ../concourse-bosh-deployment/cluster/operations/static-web.yml \
+-o ../operations/default-check-interval.yml \
 -v deployment_name="concourse" \
 -v network_name="default" \
 -v web_vm_type="default" \
@@ -14,4 +15,5 @@ bosh -e vbox deploy -d concourse ../concourse-bosh-deployment/cluster/concourse.
 -v db_persistent_disk_type="default" \
 -v external_url="http://10.244.0.34:8080" \
 -v web_ip="10.244.0.34" \
+-v default_check_interval="12h" \
 --no-redact
