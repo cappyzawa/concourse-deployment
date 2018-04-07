d78e24709e2d1296087fc5cf072c3b6d2abbe6cd
cappyzawa <m153304w@st.u-gakugei.ac.jp>
2018-04-07 15:19:47 +0900

    deployed concourse. enable to access 10.244.0.34

---
+++ vbox/deploy.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+
+bosh -e vbox deploy -d concourse ../concourse-bosh-deployment/cluster/concourse.yml \
+--vars-store ./creds.yml \
+--vars-file ../concourse-bosh-deployment/versions.yml \
+--vars-file ./passwords.yml \
+-o ../concourse-bosh-deployment/cluster/operations/basic-auth.yml \
+-o ../concourse-bosh-deployment/cluster/operations/static-web.yml \
+-v deployment_name="concourse" \
+-v network_name="default" \
+-v web_vm_type="default" \
+-v worker_vm_type="default" \
+-v db_vm_type="default" \
+-v db_persistent_disk_type="default" \
+-v external_url="http://10.244.0.34:8080" \
+-v web_ip="10.244.0.34" \
+--no-redact
